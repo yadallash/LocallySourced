3178cb55a2b77db49e2348efa167db04079337e8
Melissa He <melissahe@ac.c4q.nyc>
2018-03-03 16:32:03 -0500

    Set up part of detail VC

--- LocallySourced/LocallySourced/View Controllers/DetailViewController.swift
+++ LocallySourced/LocallySourced/View Controllers/DetailViewController.swift
@@ -7,29 +7,56 @@
 //
 
 import UIKit
+import SnapKit
+import MapKit
 
 class DetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private lazy var detailView = DetailView(frame: self.view.safeAreaLayoutGuide.layoutFrame)
+    
+    private var market = FarmersMarket()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    init(market: FarmersMarket) {
+        self.market = market
+        
+        super.init(nibName: nil, bundle: nil)
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpViews()
+    }
+    
+    private func setUpViews() {
+        detailView.mapView.delegate = self
+        
+        self.view.addSubview(detailView)
+        detailView.snp.makeConstraints { (make) in
+            make.edges.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        
+        detailView.marketNameLabel.text = market.facilityname
+        detailView.addressLabel.text = "\(market.facilitystreetname ?? "No Street Name Available"), \(market.facilitycity?.rawValue ?? "No City Name Available"), \(market.facilitystate), \(market.facilityzipcode ?? "No Zipcode Available")"
+        if let latitude = Double(market.latitude), let longitude = Double(market.longitude) {
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let region = MKCoordinateRegionMakeWithDistance(coordinate, 400, 400)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            detailView.mapView.addAnnotation(annotation)
+            detailView.mapView.setRegion(region, animated: false)
+        }
+    }
 
+}
+
+extension DetailViewController: MKMapViewDelegate {
+    
 }
