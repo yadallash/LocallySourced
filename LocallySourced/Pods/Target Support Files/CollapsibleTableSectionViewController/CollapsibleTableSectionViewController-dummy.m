363cba45690aa178290fa239f1bc7992b34fc9d2
clint-steve-m <clint_m@clintmejia.com>
2018-03-03 14:34:45 -0500

    Fix gitignore

--- LocallySourced/Pods/Target Support Files/CollapsibleTableSectionViewController/CollapsibleTableSectionViewController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CollapsibleTableSectionViewController : NSObject
-@end
-@implementation PodsDummy_CollapsibleTableSectionViewController
-@end
--- LocallySourced/Pods/Target Support Files/Pods-LocallySourcedUITests/Pods-LocallySourcedUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LocallySourcedUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LocallySourcedUITests
-@end
