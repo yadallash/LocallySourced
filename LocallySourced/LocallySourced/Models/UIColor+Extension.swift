e99de78cafe01f563164020f3ca4bea31609c7df
Melissa He <melissahe@ac.c4q.nyc>
2018-03-05 09:48:34 -0500

    Add UIColor extension

--- LocallySourced/LocallySourced/Models/UIColor+Extension.swift
+++ LocallySourced/LocallySourced/Models/UIColor+Extension.swift
@@ -10,4 +10,5 @@ import UIKit
 
 extension UIColor {
     static let buttonBlue = UIColor(red: 0.35, green: 0.71, blue: 0.98, alpha: 1)
+    static let accessGreen = UIColor(displayP3Red: 100/255, green: 180/255, blue: 130/255, alpha: 1)
 }
